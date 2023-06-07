397ace1c93369b6124e7df913ff5d616992017af
Stas Kudinov <dedbanzai91@mail.ru>
2023-06-07 11:46:16 +0300

    fix: minor changes

--- index.js
+++ index.js
@@ -1,37 +1,53 @@
-const sliderLine = document.querySelector('.slider__line');
 const buttonPrev = document.querySelector('.promo__button_prev');
 const buttonNext = document.querySelector('.promo__button_next');
-const sliderImages =document.querySelectorAll('.slider__image');
+const sliderLine = document.querySelector('.slider__line');
+const sliderImages = document.querySelectorAll('.slider__image');
 
 let offset = 0;
 let i = 0;
 
-buttonPrev.addEventListener('click', () => {
+function moveRight() {
+  offset += 337;
+  if (offset > 2040) {
+    offset = 0;
+  };
+  sliderLine.style.left = -offset + 'px';
+};
+
+function moveLeft() {
   offset -= 337;
   if (offset < 0) {
-    offset = 1388;
-  }
-  sliderLine.style.left = -offset + 'px';
-  sliderImages[i].classList.remove('slider__image_large');
-  if (i === 0) {
-    i = 4;
-  } else {
-    i--;
+    offset = 2040;
   };
-  sliderImages[i].classList.add('slider__image_large');
-});
-
-buttonNext.addEventListener('click', () => {
-  offset += 337;
-  if (offset > 1388) {
-    offset = 0;
-  }
   sliderLine.style.left = -offset + 'px';
+};
+
+function resizeRight() {
   sliderImages[i].classList.remove('slider__image_large');
-  if (i === 4) {
+  if (i === 6) {
     i = 0;
   } else {
     i++;
   };
   sliderImages[i].classList.add('slider__image_large');
+};
+
+function resizeLeft() {
+  sliderImages[i].classList.remove('slider__image_large');
+  if (i === 0) {
+    i = 6;
+  } else {
+    i--;
+  };
+  sliderImages[i].classList.add('slider__image_large');
+};
+
+buttonNext.addEventListener('click', () => {
+  moveRight();
+  resizeRight();
+});
+
+buttonPrev.addEventListener('click', () => {
+  moveLeft();
+  resizeLeft();
 });
